d50a104d4268e591fb283c51b9deb5d12f4eb385
Jason Flanagan <jasondflanagan1@outlook.com>
2021-11-15 14:16:21 -0500

    reordered react app

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,68 @@
+import { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class CampsiteInfo extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    renderCampsite(campsite) {
+
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {this.props.campsite.comments.map(comment =>
+                        <div key={comment.id}>{comment.text}<br />
+                            {comment.author} -- {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                        <br/>
+                        <br/>
+                        </div>
+                    )
+                    }
+                </div>
+            )
+        }
+        else {
+
+            return (
+                <div></div>
+            )
+        }
+    }
+
+        render() {
+            if (this.props.campsite) {
+                return (
+                    <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                    </div>
+                )
+            }
+            else {
+                return (
+                    <div/>
+                )
+            }
+        }
+    }
+
+        export default CampsiteInfo;
